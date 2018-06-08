26e4ce9903351ba178c305f945cee4ee8a7ac863
max.mehl <max.mehl@fsfe.org>
2018-06-08 16:42:53 +0200

    ignore certain keys and make it more flawless

--- site/languages/strings-to-i18n.sh
+++ site/languages/strings-to-i18n.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# strings to ignore
+ignore=("languageCode" "languageName")
+
 while read line; do
 
   # start of a new section?
   if echo "${line}" | grep -Eq "^[\s]*\[.+?\]"; then
     section=`echo "${line}" | grep -Eo "\[.+?\]" | sed -E "s|\[\|\]||g"`
-    section=`echo $section | sed -E "s|Languages\...\.||"`
+    section=`echo $section | sed -E -e "s|Languages\...\.||" -e "s|\.|_|g"`
     if [[ "$newline" -eq "y" ]]; then
       newline=n
     else
@@ -13,11 +16,14 @@ while read line; do
     fi
   elif echo "${line}" | grep -Eiq "^[\s]*[[:alnum:]]+?\s*="; then
     key=`echo "${line}" | grep -Eio "^[\s]*[[:alnum:]]+?\s*=" | sed -E -e "s|\s||g" -e "s|=||"`
-    key="$section.$key"
+    key=`echo "${section}_${key}" | sed -E "s|^Languages_.._||"`
     value=`echo "${line}" | sed -E "s|.*=\s*?(\".*\")|\1|"`
 
-    echo "[$key]"
-    echo "other= $value"
+    if [[ ! " ${ignore[@]} " =~ " ${key} " ]]; then   # if key contains string
+      echo "[$key]"
+      echo "other= $value"
+    fi
+
   elif echo "${line}" | grep -Eq "^\s*#"; then
     echo
     echo "$line"
